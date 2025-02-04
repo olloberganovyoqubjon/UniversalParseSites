e64b499b738cfd361233ca4ff3270e8e1cc26e40
olloberganovyoqubjon <yoqubjonolloberganov@gmail.com>
2025-02-04 11:50:22 +0500

    testni qo'shish

---
+++ src/test/java/uz/sites/universalparsesites/UniversalParseSitesApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.sites.universalparsesites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalParseSitesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
